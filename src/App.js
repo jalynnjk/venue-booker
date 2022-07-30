842567fed497201bbb7232d0fcde6dbe19f14194
Jalynn <jalynnking@gmail.com>
2022-07-30 19:55:18 -0400

    Map out all of request data into react cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import HomePage from './components/home_page/HomePage';
 import BookingPortal from './components/booking_portal/BookingPortal';
 import OwnerPortal from './components/owner_portal/OwnerPortal';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/booking-request' element={<BookingPortal/>}/>
-        <Route path='/owner-portal' element={<OwnerPortal />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/booking-request' element={<BookingPortal />} />
+				<Route path='/owner-portal' element={<OwnerPortal />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/components/owner_portal/OwnerPortal.js
+++ src/components/owner_portal/OwnerPortal.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 
 function OwnerPortal(props) {
 	const [bookingRequests, setBookingRequests] = useState([]);
@@ -21,18 +22,29 @@ function OwnerPortal(props) {
 		retrieveBookingRequests();
 	}, []);
 
-	return  (
+	return (
 		<main>
-            {bookingRequests ? bookingRequests.map((request, index) => {
-                return (
-                    <Card>
-                        <Card.Title>{request.client_name}</Card.Title>
-                    </Card>
-                )
-            }) : (<div>Loading requests...</div>)
-        }
+			{bookingRequests ? (
+				bookingRequests.map((request, index) => {
+					return (
+						<Card key={index}>
+							<Card.Title>{request.client_name}</Card.Title>
+							<Card.Subtitle>{request.client_email}</Card.Subtitle>
+							<Card.Text>{request.wedding_date}</Card.Text>
+							<Card.Text>{request.number_guests}</Card.Text>
+							<Card.Text>{request.budget}</Card.Text>
+							<Card.Text>{request.ceremony_location}</Card.Text>
+							<Card.Text>{request.reception_location}</Card.Text>
+                            <Button>Accept</Button>
+                            <Button>Decline</Button>
+						</Card>
+					);
+				})
+			) : (
+				<div>Loading requests...</div>
+			)}
 		</main>
-
-    )}
+	);
+}
 
 export default OwnerPortal;
