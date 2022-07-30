649a918cf9c7fc3a4041c75f4e88ecb4e2d69362
Jalynn <jalynnking@gmail.com>
2022-07-30 10:55:15 -0400

    Map booking request data into html

--- src/components/owner_portal/OwnerPortal.js
+++ src/components/owner_portal/OwnerPortal.js
@@ -1,23 +1,38 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
 
 function OwnerPortal(props) {
 	const [bookingRequests, setBookingRequests] = useState([]);
+
 	async function retrieveBookingRequests() {
 		try {
 			const response = await axios.get(
 				'http://localhost:8000/api/booking_requests'
 			);
-            setBookingRequests(response)
-            console.log(bookingRequests)
+			setBookingRequests(response.data);
+			console.log(bookingRequests);
 		} catch (error) {
-            console.log(error)
-        }
+			console.log(error);
+		}
 	}
+
 	useEffect(() => {
 		retrieveBookingRequests();
 	}, []);
-	return <div></div>;
-}
+
+	return  (
+		<main>
+            {bookingRequests ? bookingRequests.map((request, index) => {
+                return (
+                    <Card>
+                        <Card.Title>{request.client_name}</Card.Title>
+                    </Card>
+                )
+            }) : (<div>Loading requests...</div>)
+        }
+		</main>
+
+    )}
 
 export default OwnerPortal;
